9eacb00be3a7d339973bd469ab910b42358669e5
zth <380041632@qq.com>
2020-04-13 09:23:10 +0800

    about socket project

--- Java学习/java_learning_project/TestSocket/src/socket/serverclient/Client.java
+++ Java学习/java_learning_project/TestSocket/src/socket/serverclient/Client.java
@@ -1,15 +1,39 @@
 package socket.serverclient;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String args[]){
-        try{
-            //连接到本机的8888端口
+
+    public static void main(String[] args) {
+
+        try {
             Socket s = new Socket("127.0.0.1", 8888);
-            System.out.println(s);
-            s.close();
-        }catch (Exception e){
+
+            OutputStream os = s.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(os);
+            InputStream is = s.getInputStream();
+            DataInputStream dis = new DataInputStream(is);
+
+            while(true){
+                Scanner sc = new Scanner(System.in);
+                String str = sc.next();
+                dos.writeUTF(str);
+                String msg = dis.readUTF();
+                System.out.println("收到服务端信息"+msg);
+            }
+
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
--- Java学习/java_learning_project/TestSocket/src/socket/serverclient/Server.java
+++ Java学习/java_learning_project/TestSocket/src/socket/serverclient/Server.java
@@ -5,24 +5,40 @@
 
 package socket.serverclient;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
-    public static void main(String args[]){
-        try{
-            //服务端打开端口8888
-            ServerSocket ss =  new ServerSocket(8888);
 
-            //在8888端口上监听，看是否有连接请求过来
-            System.out.println("监听在8888端口");
+    public static void main(String[] args) {
+        try {
+
+            ServerSocket ss = new ServerSocket(8888);
+
+            System.out.println("监听在端口号:8888");
             Socket s = ss.accept();
 
-            System.out.println("有连接过来 " + s);
+            InputStream is = s.getInputStream();
+            DataInputStream dis = new DataInputStream(is);
+            OutputStream os = s.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(os);
 
-            s.close();
-            ss.close();
-        }catch (Exception e){
+            while (true) {
+                String msg = dis.readUTF();
+                System.out.println("收到客户端信息"+msg);
+                Scanner sc = new Scanner(System.in);
+                String str = sc.next();
+                dos.writeUTF(str);
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
