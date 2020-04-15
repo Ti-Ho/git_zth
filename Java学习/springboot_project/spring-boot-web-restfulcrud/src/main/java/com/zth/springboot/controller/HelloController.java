e0beb39d4cefbff8e6f74a7b514febe6a0046fe1
zth <380041632@qq.com>
2020-04-16 00:20:19 +0800

    create springboot web project

---
+++ Java学习/springboot_project/spring-boot-web-restfulcrud/src/main/java/com/zth/springboot/controller/HelloController.java
@@ -0,0 +1,8 @@
+package com.zth.springboot.controller;
+
+/**
+ * @author Kai
+ * @date 2020/4/16 0:18
+ */
+public class HelloController {
+}
---
+++ Java学习/springboot_project/spring-boot-web-restfulcrud/src/test/java/com/zth/springboot/SpringBootWebRestfulcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zth.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebRestfulcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
