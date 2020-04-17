1290d3cfe713569b2b487557b6841a62fa738776
zth <380041632@qq.com>
2020-04-17 16:20:42 +0800

    none

---
+++ Java学习/springboot_project/mybatis-restful-demo/src/main/java/com/zth/springboot/MybatisRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.zth.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisRestfulApplication.class, args);
+    }
+
+}
---
+++ Java学习/springboot_project/mybatis-restful-demo/src/test/java/com/zth/springboot/MybatisRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zth.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
