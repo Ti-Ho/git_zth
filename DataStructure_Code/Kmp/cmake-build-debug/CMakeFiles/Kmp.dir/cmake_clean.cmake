bf1fc4c5ea10c825032a78a993bedcf65cca934f
zth <380041632@qq.com>
2020-07-06 22:32:20 +0800

    KMP

---
+++ DataStructure_Code/Kmp/cmake-build-debug/CMakeFiles/Kmp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kmp.dir/main.cpp.obj"
+  "Kmp.pdb"
+  "Kmp.exe"
+  "Kmp.exe.manifest"
+  "libKmp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kmp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
